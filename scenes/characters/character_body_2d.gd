4d60cbad8d20298177b2a563654e3798f61c2a4f
Alexander <sasha.timofeev19999@mail.ru>
2024-04-30 16:53:28 +0300

    Initial commit

---
+++ scenes/characters/character_body_2d.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -750.0
+@onready var sprite = $Sprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		sprite.scale.x = abs(sprite.scale.x) * -1
+	
+	if Input.is_action_pressed("right"):
+		sprite.scale.x = abs(sprite.scale.x)
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 12)
+
+	move_and_slide()
+	update_animation()
+	
+
+func update_animation() -> void:
+	if (velocity.y > 0):
+		sprite.animation = "falling"
+		return
+	
+	if (velocity.y < 0):
+		sprite.animation = "jumping"
+		return
+	
+	if (velocity.x != 0):
+		sprite.animation = "running"
+		return
+	
+	sprite.animation = "idle"
+
---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
